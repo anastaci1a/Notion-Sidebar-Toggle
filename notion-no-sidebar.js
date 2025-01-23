915e954112517fc7155a9281ea652b10b74e11f4
Ana Jahnel <anastaciajahnel@gmail.com>
2025-01-23 03:40:00 -0500

    Update notion-no-sidebar.js

--- notion-no-sidebar.js
+++ notion-no-sidebar.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         notion-change-sidebar-behaviour
-// @namespace    https://github.com/Roznoshchik/Notion-no-sidebar/
-// @version      0.1
-// @description  Notions auto appearing sidebar is very annoying when working side by side with other windows. This code inserts a button to the dom and uses it to change the sidebar behaviour.
-// @author       Roznoshchik
+// @name         Notion - Sidebar Toggle Button
+// @namespace    https://github.com/anastaci1a/Notion-Sidebar-Toggle
+// @version      1.0.0
+// @description  Adds a button to the Notion dom which toggles the sidebar visibility
+// @author       Roznoshchik, forked by anastaci1a
 // @match        https://www.notion.so/*
 // @grant        none
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
@@ -16,92 +16,116 @@ let alreadyCalled = null;
 (function () {
     'use strict';
 
-    //set global variables
-    let btn, NScontainer, Nframe, Nsidebar, Ntopbar;
+    // settings
+    let hideOnStart = true;
 
+    // sidebar-adjacent vars
+    let sidebarEnable = true;
+    let NshowSidebarButton, NScontainer, Nframe, Nsidebar, Ntopbar;
 
+    // toggle button vars
+    let toggleButton, toggleButton_exists = false;
+    const toggleButton_innerHTML_enabled  = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#d2d2d2" class="size-6"><path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" /></svg>';
+    const toggleButton_innerHTML_disabled = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#d2d2d2" class="size-6"><path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" /><path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" /></svg>';
 
-    //Because Notion creates the page from JS, need to use this function to wait for everything to load.
-    function a() {
-        waitForKeyElements(".notion-sidebar-container", b);
-    };
-    function b() {
-        waitForKeyElements(".notion-frame", c);
-    };
-    function c() {
-        waitForKeyElements(".notion-topbar", d);
-    };
-    function d() {
-        waitForKeyElements(".notion-topbar > div", setVariables);
-    };
+    // ~
+
+    // because Notion creates the page from JS, script must for elements
     waitForKeyElements(".notion-sidebar", a);
+    function a() { waitForKeyElements(".notion-sidebar-container", b); }
+    function b() { waitForKeyElements(".notion-frame", c); }
+    function c() { waitForKeyElements(".notion-topbar", d); }
+    function d() { waitForKeyElements(".notion-topbar > div", e); }
+    function e() { waitForKeyElements(".notion-open-sidebar", setVariables); }
 
-
-    //Once everything is loaded, query everything and set initial values.
+    // after loading, query everything and set initial values
     function setVariables() {
         NScontainer = document.querySelector(".notion-sidebar-container");
-        NScontainer.style.visibility = "visible";
-
         Nframe = document.querySelector(".notion-frame");
-        Nframe.style.position = "relative";
-
         Nsidebar = document.querySelector(".notion-sidebar");
-        Nsidebar.style.visibility = "visible";
-
         Ntopbar = document.querySelector(".notion-topbar");
+        NshowSidebarButton = document.querySelector(".notion-open-sidebar");
 
+        // load main function
+        addToggleToDOM();
 
-        //call the main function.
-        isLoaded();
+        // hide on first load
+        if (hideOnStart) {
+            toggleSidebar();
+            hideOnStart = false;
+        }
     };
 
-    //function that changes the sidebar position and visibility on button click.
-    function vizsidebar() {
-
-        if (NScontainer.style.visibility === "visible") {
+    // change the sidebar position and visibility on button click
+    function toggleSidebar() {
+        // hide the damn thing
+        if (sidebarEnable) {
+            // hide sidebar
             NScontainer.style.visibility = "hidden";
-        }
-        else {
-            NScontainer.style.visibility = "visible";
+            Nsidebar.style.display = "none";
+
+            // hide the "normal" sidebar toggle
+            NshowSidebarButton.parentElement.parentElement.style.width = "0px";
+            NshowSidebarButton.parentElement.parentElement.style.scale = "0";
+            document.querySelector(".hamburgerMenu").style.visibility = "hidden";
+            NshowSidebarButton.style.visibility = "hidden";
+
+            // set toggle icon
+            toggleButton.innerHTML = toggleButton_innerHTML_enabled;
         }
 
-        if (Nsidebar.style.display === "flex") {
-            Nsidebar.style.display = "none";
-        }
+        // re-enable sidebar
         else {
+            // show sidebar
+            NScontainer.style.visibility = "visible";
+            NshowSidebarButton.style.visibility = "visible";
             Nsidebar.style.display = "flex";
 
+            // hide the "normal" sidebar toggle
+            NshowSidebarButton.parentElement.parentElement.style.width = "48px";
+            NshowSidebarButton.parentElement.parentElement.style.scale = "1";
+            document.querySelector(".hamburgerMenu").style.visibility = "visible";
+            NshowSidebarButton.style.visibility = "visible";
+
+            // set toggle icon
+            toggleButton.innerHTML = toggleButton_innerHTML_disabled;
         }
 
-        if (Nframe.style.position === "relative") {
-            Nframe.style.position = "absolute";
-            Nframe.style.width = "100vw";
-            Nframe.style.flexGrow = "1";
-        }
-        else {
-            Nframe.style.position = "relative";
-            Nframe.style.flexGrow = "0";
-
-        }
+        // toggle
+        sidebarEnable = !sidebarEnable;
     };
 
-    //main function that creates the button and inserts it into the dom
-    function isLoaded() {
-        if (alreadyCalled) return;
-        else alreadyCalled = true;
+    // main function that creates the button and inserts it into the dom
+    function addToggleToDOM() {
+        // add toggle if it doesn't exist (yet!!)
+        if (!toggleButton_exists) {
+            // toggle html config
+            toggleButton = document.createElement("button");
+            toggleButton.innerHTML = toggleButton_innerHTML_disabled;
 
-        btn = document.createElement("button");
-        btn.innerHTML = '<svg viewBox="0 0 14 14" style="width: 14px; height: 14px; display: block; fill: rgba(55, 53, 47, 0.8); flex-shrink: 0; backface-visibility: hidden;" class="doubleChevronLeft"><path d="M7 12.025L8.225 13.25L14 7.125L8.225 1L7 2.225L11.55 7.125L7 12.025ZM0 12.025L1.225 13.25L7 7.125L1.225 1L8.56743e-07 2.225L4.55 7.125L0 12.025Z"></path></svg>';
-        btn.classList.add('NOTION-SIDEBAR-HACK');
-        btn.style.cssText = "background-color: #f4511e; border: none; color: white; padding: 8px 8px; text-align: center; font-size: 16px; margin: 4px 2px; opacity: 0.6; transition: 0.3s; display: inline-block; text-decoration: none; cursor: pointer;"
+            toggleButton.classList.add('NOTION-SIDEBAR-TOGGLE');
+            toggleButton.style.cssText = "background-color: transparent; border: none; flex-shrink: 0; width: 48px; height: 48px; margin: -12px -6px -12px -12px; padding: 12px;"
 
-        const child = Ntopbar.firstElementChild;
+            // click behavior
+            toggleButton.onclick = () => toggleSidebar();
 
-        child ? child.insertBefore(btn, child.firstChild) : Ntopbar.insertBefore(btn, Ntopbar.firstChild);
+            // actually add the toggle to dom
+            const child = Ntopbar.firstElementChild.firstElementChild;
+            child ? child.insertBefore(toggleButton, child.firstChild) : Ntopbar.insertBefore(toggleButton, Ntopbar.firstChild);
+            toggleButton_exists = true;
+        }
 
-        // sets the onclick function for the button. 
-        btn.onclick = function () {
-            vizsidebar();
-        };
+        // if sidebar is opened, remove toggle (should only happen when toggle is inactive)
+        waitForKeyElements(".notion-close-sidebar", () => {
+            // remove
+            toggleButton.remove();
+            toggleButton_exists = false;
+
+            // if sidebar is closed, re-add toggle
+            const closeSidebarButton = document.querySelector(".notion-close-sidebar");
+            closeSidebarButton.addEventListener("click", () => {
+                a();
+            });
+        });
     };
 })();
