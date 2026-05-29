e5ae88518f49b0a3a23f819af90355ed20ee7896
Ana Jahnel <48846277+anastaci1a@users.noreply.github.com>
2026-05-29 00:30:36 -0400

    app.notion.com support / major refactoring

--- notion-sidebar-toggle.user.js
+++ notion-sidebar-toggle.user.js
@@ -1,131 +1,306 @@
 // ==UserScript==
 // @name         Notion - Sidebar Toggle Button
 // @namespace    https://github.com/anastaci1a/Notion-Sidebar-Toggle
-// @version      1.0.0
+// @version      1.1.0
 // @description  Adds a button to the Notion dom which toggles the sidebar visibility
 // @author       Roznoshchik, forked by anastaci1a
-// @match        https://www.notion.so/*
+// @match        https://app.notion.com/*
 // @grant        none
-// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @require      https://gist.githubusercontent.com/Anemone95/d133848ae16055ea0efc5d0f45d33e1e/raw/a0f48d1c1664afc17bc81a5dd83337a16c9dea31/waitForKeyElements.js
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
-// @run-at document-idle
+// @run-at       document-start
 // ==/UserScript==
-
-// config
-let hideOnStart = true;
 
 (function () {
     'use strict';
 
-    // sidebar-adjacent vars
-    let sidebarDisable = false;
-    let NshowSidebarButton, NScontainer, Nframe, Nsidebar, Ntopbar;
+    // config
 
-    // toggle button vars
-    let toggleButton, toggleButton_exists = false;
-    const toggleButton_innerHTML_enabled  = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#d2d2d2" class="size-6"><path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" /></svg>';
-    const toggleButton_innerHTML_disabled = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#d2d2d2" class="size-6"><path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" /><path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" /></svg>';
+    const FORCE_HIDE_ON_START = false;
+    const DEBUG_LOG_ENABLE    = false;
 
-    // toggle button spacer vars
-    // let toggleButtonSpacer;
-    // const toggleButtonSpacer_innerHTML = '';
+    // debug
 
-    // ~
+    const DEBUG_LOG_FORMAT = '•—––· 🪰 SIDEBAR TOGGLE DEBUG 🪲 ·––—•\n\n%s\n\n•—————————————————————————————————————•';
 
-    // because Notion creates the page from JS, script must wait for elements
-    waitForKeyElements(".notion-sidebar", a);
-    function a() { waitForKeyElements(".notion-sidebar-container", b); }
-    function b() { waitForKeyElements(".notion-frame", c); }
-    function c() { waitForKeyElements(".notion-topbar", d); }
-    function d() { waitForKeyElements(".notion-topbar > div", e); }
-    function e() { waitForKeyElements(".notion-open-sidebar", setVariables); }
+    function debugLog(...obj) {
+        const LINE_START = "> "
 
-    // after loading, query everything and set initial values
-    function setVariables() {
-        NScontainer = document.querySelector(".notion-sidebar-container");
-        Nframe = document.querySelector(".notion-frame");
-        Nsidebar = document.querySelector(".notion-sidebar");
-        Ntopbar = document.querySelector(".notion-topbar");
-        NshowSidebarButton = document.querySelector(".notion-open-sidebar");
+        if (!DEBUG_LOG_ENABLE) return;
+        const msg_body = obj.join('\n').replace(/(?<=^|\n)/g, LINE_START);
+        console.log(
+            DEBUG_LOG_FORMAT, msg_body
+        );
+    }
 
-        // load main function
-        addToggleToDOM();
+    // notion elems
 
-        // hide on first load
-        if (hideOnStart) {
-            toggleSidebar();
-            hideOnStart = false;
+    const JQ_ELEM_TOPBAR        = '.notion-topbar';
+    const JQ_ELEMS_TOPBAR_INNER = `${JQ_ELEM_TOPBAR} > div > div`;
+
+    const JQ_ELEM_BT_SIDEBAR_CLOSE  = '.notion-sidebar-switcher .notion-close-sidebar';
+    const JQ_ELEM_BT_SIDEBAR_OPEN   = '.notion-open-sidebar';
+    const JQ_ELEM_BT_SIDEBAR_OPEN_2 = '.notion-sidebar [aria-label="Lock sidebar open"]';
+
+    const JQ_ELEMS_LAYOUT_AWAIT = [
+        '.notion-sidebar-container',
+        '.notion-sidebar',
+        '.notion-frame',
+        JQ_ELEM_TOPBAR,
+        JQ_ELEMS_TOPBAR_INNER
+    ];
+
+    // toggle button
+
+    const CLASS_BT_TOGGLE = 'sidebar-visible-toggle';
+
+    const SVG_BT_TOGGLE_ENABLED  = /* crossed out eye */ `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#d2d2d2" class="size-6"><path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" /></svg>`;
+    const SVG_BT_TOGGLE_DISABLED = /* normal eye      */ `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#d2d2d2" class="size-6"><path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" /><path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" /></svg>`;
+
+    const CSS_BT_TOGGLE_ENABLED  = `background-color: transparent; border: none; flex-shrink: 0; width: 48px; height: 48px; margin: -12px -3px -12px -12px; padding: 12px;`;
+    const CSS_BT_TOGGLE_DISABLED = CSS_BT_TOGGLE_ENABLED;
+
+    const BT_TOGGLE = {
+        elem: (() => {
+            const bt = document.createElement("button");
+            bt.classList.add(CLASS_BT_TOGGLE);
+            bt.onclick = () => BT_TOGGLE.toggle();
+
+            return bt;
+        })(),
+
+        enabled: undefined,
+
+        toggle() {
+            if (this.enabled) {
+                this.setDisabled();
+            } else {
+                this.setEnabled();
+            }
+        },
+
+        set(enabled) {
+          enabled ? this.setEnabled() : this.setDisabled()
+        },
+
+        setEnabled() {
+            this.enabled = true;
+            this.setVis(
+                SVG_BT_TOGGLE_ENABLED,
+                CSS_BT_TOGGLE_ENABLED
+            );
+            sidebarHide();
+        },
+
+        setDisabled() {
+            this.enabled = false;
+            this.setVis(
+                SVG_BT_TOGGLE_DISABLED,
+                CSS_BT_TOGGLE_DISABLED
+            );
+            sidebarShow();
+        },
+
+        setVis(html, css) {
+            this.elem.innerHTML     = html;
+            this.elem.style.cssText = css;
         }
     };
 
-    // change the sidebar position and visibility on button click
-    function toggleSidebar() {
-        // toggle
-        sidebarDisable = !sidebarDisable;
+    // detect and handle sidebar state (whether it is open or not)
 
-        // hide the damn thing
-        if (sidebarDisable) {
-            // hide sidebar
-            NScontainer.style.visibility = "hidden";
-            Nsidebar.style.display = "none";
+    debugLog('Waiting for page to generate...');
+    waitForElems(JQ_ELEMS_LAYOUT_AWAIT, () => {
+        debugLog('Necessary elements detected.', 'Handling sidebar state...');
+        detectSidebarState(
+            () => {
+                handleSidebarOpened();
+                manageSidebarOpened();
+            }, () => {
+                handleSidebarClosed();
+                manageSidebarClosed();
+            }
+        );
+    });
 
-            // hide the "normal" sidebar toggle (hamburger menu)
-            NshowSidebarButton.style.visibility = "hidden";
-            NshowSidebarButton.parentElement.parentElement.style.visibility = "hidden";
-            NshowSidebarButton.parentElement.parentElement.parentElement.style.width = "25px";
+    function detectSidebarOpened(handle) {
+        waitForElem(JQ_ELEM_BT_SIDEBAR_CLOSE, handle);
+    }
 
-            // set toggle icon
-            toggleButton.innerHTML = toggleButton_innerHTML_enabled;
+    function detectSidebarClosed(handle) {
+        waitForElem(JQ_ELEM_BT_SIDEBAR_OPEN, handle);
+    }
+
+    function detectSidebarState(handleOpened, handleClosed) {
+        const detectState = new Promise((resolve, reject) => {
+            detectSidebarOpened(resolve);
+            detectSidebarClosed(reject);
+        });
+
+        detectState.then(handleOpened, handleClosed);
+    }
+
+    function handleSidebarOpened() {
+        debugLog('[handleSidebarOpened]', 'Removing sidebar toggle button...');
+
+        BT_TOGGLE.elem.remove(); // remove toggle
+    }
+
+    function handleSidebarClosed() {
+        debugLog('[handleSidebarClosed]', 'Adding sidebar toggle button...');
+
+        sidebarInjectToggle(); // add toggle
+    }
+
+    async function manageSidebarClosed() {
+        // debugLog('[manageSidebarClosed]', 'Waiting for open sidebar buttons...');
+
+        if (!BT_TOGGLE.enabled) { // if sidebar is visible, await closing
+            waitForElemsLive([JQ_ELEM_BT_SIDEBAR_OPEN, JQ_ELEM_BT_SIDEBAR_OPEN_2], () => {
+                debugLog('[manageSidebarClosed]', 'Waiting for sidebar to open...');
+
+                const btsSidebarOpen = [
+                    document.querySelector(JQ_ELEM_BT_SIDEBAR_OPEN),
+                    document.querySelector(JQ_ELEM_BT_SIDEBAR_OPEN_2)
+                ];
+
+                for (const bt of btsSidebarOpen) {
+                    bt.addEventListener("click", () => {
+                        debugLog('[manageSidebarClosed]', 'Sidebar was opened!');
+
+                        handleSidebarOpened();
+                        manageSidebarOpened();
+                    });
+                }
+            });
         }
+    }
 
-        // re-enable sidebar
-        else {
-            // show sidebar
-            NScontainer.style.visibility = "visible";
-            NshowSidebarButton.style.visibility = "visible";
-            Nsidebar.style.display = "flex";
+    async function manageSidebarOpened() {
+        // debugLog('[manageSidebarOpened]', 'Waiting for close sidebar button...');
 
-            // show the "normal" sidebar toggle (hamburger menu)
-            NshowSidebarButton.style.visibility = "visible";
-            NshowSidebarButton.parentElement.parentElement.style.visibility = "visible";
-            NshowSidebarButton.parentElement.parentElement.parentElement.style.width = "56px";
+        waitForElemLive(JQ_ELEM_BT_SIDEBAR_CLOSE, () => {
+            debugLog('[manageSidebarOpened]', 'Waiting for sidebar to close...');
 
-            // set toggle icon
-            toggleButton.innerHTML = toggleButton_innerHTML_disabled;
-        }
-    };
+            const btSidebarClose = document.querySelector(JQ_ELEM_BT_SIDEBAR_CLOSE);
+            btSidebarClose.addEventListener("click", () => {
+                debugLog('[manageSidebarOpened]', 'Sidebar was closed!');
 
-    // main function that creates the button and inserts it into the dom
-    function addToggleToDOM() {
-        // add toggle if it doesn't exist (yet!!)
-        if (!toggleButton_exists) {
-            // toggle html config
-            toggleButton = document.createElement("button");
-            toggleButton.innerHTML = toggleButton_innerHTML_disabled;
-
-            toggleButton.classList.add('notion-sidebar-toggle');
-            toggleButton.style.cssText = "background-color: transparent; border: none; flex-shrink: 0; width: 48px; height: 48px; margin: -12px -3px -12px -12px; padding: 12px;"
-
-            // click behavior
-            toggleButton.onclick = () => toggleSidebar();
-
-            // actually add the toggle to dom
-            const child = Ntopbar.firstElementChild.firstElementChild;
-            (child ? child : Ntopbar).insertBefore(toggleButton, (child ? child : Ntopbar).firstChild);
-            toggleButton_exists = true;
-        }
-
-        // if sidebar is opened, remove toggle (should only happen when toggle is inactive)
-        waitForKeyElements(".notion-close-sidebar", () => {
-            // remove
-            toggleButton.remove();
-            toggleButton_exists = false;
-
-            // if sidebar is closed, re-add toggle
-            const closeSidebarButton = document.querySelector(".notion-close-sidebar");
-            closeSidebarButton.addEventListener("click", () => {
-                a();
+                handleSidebarClosed();
+                manageSidebarClosed();
             });
         });
-    };
+    }
+
+    // sidebar force hide/show (custom toggle)
+
+    function sidebarInjectToggle() { // assumes sidebar is closed
+        const topbar = document.querySelector(JQ_ELEMS_TOPBAR_INNER);
+
+        if (typeof BT_TOGGLE.enabled == 'undefined') {
+            BT_TOGGLE.set(FORCE_HIDE_ON_START);
+        }
+
+        topbar.insertBefore(
+            BT_TOGGLE.elem,
+            topbar.firstChild
+        );
+    }
+
+    function sidebarHide() {
+        waitForElemsLive(JQ_ELEMS_LAYOUT_AWAIT, () => {
+            debugLog('[sidebarHide]', 'HIDING SIDEBAR!!!');
+
+            const btSidebarOpen = document.querySelector(".notion-open-sidebar");
+            const sidebarContainer = document.querySelector(".notion-sidebar-container");
+            const sidebar = document.querySelector(".notion-sidebar");
+            const frame = document.querySelector(".notion-frame");
+            const topbar = document.querySelector(".notion-topbar");
+
+            // hide sidebar
+            sidebarContainer.style.visibility = "hidden";
+            sidebar.style.display = "none";
+
+            // hide the "normal" sidebar toggle (hamburger menu)
+            btSidebarOpen.style.visibility = "hidden";
+            btSidebarOpen.parentElement.parentElement.style.visibility = "hidden";
+            btSidebarOpen.parentElement.parentElement.parentElement.style.width = "25px";
+        });
+    }
+
+    function sidebarShow() {
+        waitForElemsLive(JQ_ELEMS_LAYOUT_AWAIT, () => {
+            debugLog('[sidebarShow]', 'SHOWING SIDEBAR!');
+
+            const btSidebarOpen = document.querySelector(".notion-open-sidebar");
+            const sidebarContainer = document.querySelector(".notion-sidebar-container");
+            const sidebar = document.querySelector(".notion-sidebar");
+            const frame = document.querySelector(".notion-frame");
+            const topbar = document.querySelector(".notion-topbar");
+
+            // show sidebar
+            sidebarContainer.style.visibility = "visible";
+            sidebar.style.display = "flex";
+
+            // show the "normal" sidebar toggle (hamburger menu)
+            btSidebarOpen.style.visibility = "visible";
+            btSidebarOpen.parentElement.parentElement.style.visibility = "visible";
+            btSidebarOpen.parentElement.parentElement.parentElement.style.width = "56px";
+
+            // reassign button event listener(s)
+            manageSidebarClosed();
+        });
+    }
+
+    // util
+
+    function waitForElem(elemSelector, callback) {
+        waitForKeyElements(elemSelector, (context) => {
+            callback.apply(context);
+        });
+    }
+
+    function waitForElems(elemSelectors, callback) {
+        if (typeof elemSelectors == 'undefined' || elemSelectors.length == 0) {
+            callback.apply(this);
+            return;
+        }
+
+        waitForElem(elemSelectors[0], () => {
+            elemSelectors.shift();
+            waitForElems(elemSelectors, callback);
+        })
+    }
+
+    function waitForElemLive(elemSelector, callback) {
+        const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+        const wait = new Promise(async (resolve) => {
+            while (true) {
+                const found = document.querySelector(elemSelector);
+                if (typeof found != 'undefined' && found !== null) {
+                    // debugLog(`Found: (${elemSelector}):`, found);
+                    resolve(found);
+                    break;
+                }
+                await delay(200);
+            }
+        });
+
+        wait.then(() => {
+            callback.apply(this);
+        });
+    }
+
+    function waitForElemsLive(elemSelectors, callback) {
+        if (typeof elemSelectors == 'undefined' || elemSelectors.length == 0) {
+            callback.apply(this);
+            return;
+        }
+
+        waitForElemLive(elemSelectors[0], () => {
+            elemSelectors.shift();
+            waitForElemsLive(elemSelectors, callback);
+        })
+    }
 })();
